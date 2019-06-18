de32638588300b7115936a2667cbd72766450206
huberth <huberth@umich.edu>
2019-06-18 14:23:18 -0400

    Added service worker.

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const CACHE_NAME = "static-cache-v1";
+
+const FILES_TO_CACHE = [
+    "offline.html"
+];
+
+self.addEventListener ("install", event => {
+    console.log ("Install");
+
+    self.skipWaiting ();
+})
+
+self.addEventListener ("activate", event => {
+    console.log ("Activate");
+
+    self.clients.claim ();
+})
+
+self.addEventListener ("fetch", event => {
+    console.log ("Fetch", event.request.url);
+})
